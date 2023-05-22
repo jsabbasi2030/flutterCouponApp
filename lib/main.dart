65cc30f98314848fb7ceca02640b811a43c28a5b
Student <lab-205@aptech-msg.edu>
2023-05-22 14:50:53 +0500

    First Project On Flutter coupons hub

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:coupons/users/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Coupon Discount Hub',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: FutureBuilder(
+        
+        builder: (context, dataSnapShot){
+
+          return Loginscreen();
+        }
+        
+        ),
+    );
+  }
+
+}
+
+  
---
+++ lib/users/login_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Loginscreen extends StatefulWidget {
+  const Loginscreen({super.key});
+
+  @override
+  State<Loginscreen> createState() => _LoginscreenState();
+}
+
+class _LoginscreenState extends State<Loginscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+       
+       title: Text('LoginScreen'),
+
+      ),
+
+
+
+
+
+    );
+  }
+}
